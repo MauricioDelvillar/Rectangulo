ff3fe32abf20f7666c1109316473362065d2a1a3
Mauricio Mauricio <pedro.delvillar@tecsup.edu.pe>
2022-03-30 22:42:43 -0500

    Agregar archivos de proyecto.

---
+++ Rectangulo/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoRectangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona = new Persona();
+            Console.WriteLine("Ingrese su nombre:");
+            persona.Nombres = Console.ReadLine();
+            Console.WriteLine("Ingrese su apellido:");
+            persona.Apellidos = Console.ReadLine();
+
+            string nombresCompletos;
+            nombresCompletos = persona.DevolverNombresCompletos();
+            Console.WriteLine("__________________________");
+            Console.WriteLine("     Nombres completos    ");
+            Console.WriteLine("__________________________");
+            Console.WriteLine(nombresCompletos);
+            Console.WriteLine("__________________________");
+
+
+
+            Rectangulo rectangulo = new Rectangulo();
+            // Coordenada 1 (1;3)
+
+            Console.WriteLine("Ingrese coordenada X1:");
+            rectangulo.x1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Ingrese coordenada Y1:");
+            rectangulo.y1 = Convert.ToDouble(Console.ReadLine());
+
+            // Coordenada 2 (4;0)
+            Console.WriteLine("Ingrese coordenada X2:");
+            rectangulo.x2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Ingrese coordenada Y2:");
+            rectangulo.y2 = Convert.ToDouble(Console.ReadLine());
+
+            // Coordenada 3 (10;5)
+            Console.WriteLine("Ingrese coordenada X3:");
+            rectangulo.x3 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Ingrese coordenada Y3:");
+            rectangulo.y3 = Convert.ToDouble(Console.ReadLine());
+
+            // Coordenada 4 (6.8)
+            Console.WriteLine("Ingrese coordenada X4:");
+            rectangulo.x4 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Ingrese coordenada Y4:");
+            rectangulo.y4 = Convert.ToDouble(Console.ReadLine());
+
+
+            Console.WriteLine("Area redondeada del rectangulo:");
+            Console.WriteLine(rectangulo.Area(rectangulo.x1, rectangulo.y1, rectangulo.x2, rectangulo.y2, rectangulo.x3, rectangulo.y3, rectangulo.x4, rectangulo.y4));
+            Console.WriteLine("Perimetro redondeada del rectangulo:");
+            Console.WriteLine(rectangulo.Perimetro(rectangulo.x1, rectangulo.y1, rectangulo.x2, rectangulo.y2, rectangulo.x3, rectangulo.y3, rectangulo.x4, rectangulo.y4));
+            Console.Read();
+
+
+        }
+    }
+}
---
+++ Rectangulo/Rectangulo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoRectangulo
+{
+    public class Rectangulo
+    {
+        public double x1 { get; set; }
+        public double y1 { get; set; }
+
+        public double x2 { get; set; }
+        public double y2 { get; set; }
+
+        public double x3 { get; set; }
+        public double y3 { get; set; }
+
+        public double x4 { get; set; }
+        public double y4 { get; set; }
+
+
+        public double Area(double x1, double y1, double x2, double y2, double x3, double y3, double x4, double y4)
+        {
+            double u = Math.Abs(x2 - x1);
+            double v = Math.Abs(y3 - y1);
+            double area = u * v;
+            return area;
+        }
+        public double Perimetro(double x1, double y1, double x2, double y2, double x3, double y3, double x4, double y4)
+        {
+            double perimetro = (2 * (x2 - x1) + 2 * (y3 - y1));
+            return perimetro;
+        }
+
+
+    }
+}
